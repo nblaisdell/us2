79a0f341d71a918b946c9d8e4b1e24781fc742b9
Nathan Blaisdell <nblaisdell@gmail.com>
2013-06-02 15:36:24 -0500

    adding code

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php $pagetitle = "home"; ?>
+<?php require_once("incl/functions.php"); ?>
+<?php include("incl/header.php"); ?>
+
+    
+
+<?php include("incl/footer.php"); ?>
---
+++ staff.php
@@ -0,0 +1,7 @@
+<?php $pagetitle = "admin"; ?>
+<?php require_once("incl/functions.php"); ?>
+<?php include("incl/header.php"); ?>
+
+    
+
+<?php include("incl/footer.php"); ?>
